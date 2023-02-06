25eb2a24457335bb66afd50997cc187388011625
Alexandru Racicovschi <a18596@d1-08-05.dped.local>
2023-02-06 16:11:18 +0000

    Criação do Repositório local.

--- src/main/java/com/mycompany/projetogithub/ProjetoGitHub.java
+++ src/main/java/com/mycompany/projetogithub/ProjetoGitHub.java
@@ -3,6 +3,7 @@ package com.mycompany.projetogithub;
 public class ProjetoGitHub {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("RACCCIIIIIIII!!!!!!!");
+        //falta um comentario...
     }
 }
